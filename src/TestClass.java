2676045590a0f2216ce6aad44d692132f70b0c3d
Anna Fedorova <snysmymric@gmail.com>
2022-04-14 11:20:40 -0700

    Change parametrs name in method print andStr printlnStr

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,10 +1,10 @@
 public class TestClass {
 
-    public static void printlnStr (String str) {
-        System.out.println(str);
+    public static void printlnStr (String string) {
+        System.out.println(string);
     }
-    public static void printStr (String str) {
-        System.out.print(str + " ");
+    public static void printStr (String string) {
+        System.out.print(string + " ");
     }
 
     public static void main(String[] args) {
